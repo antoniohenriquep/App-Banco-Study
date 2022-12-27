16893bd37654a08f38be0645d97b67ad70911666
Antonio Pedreira <antoniohenriquep@outlook.com>
2022-12-26 23:09:26 -0300

    Implementando o Picker de genero

--- App.js
+++ App.js
@@ -10,14 +10,13 @@ export default function App()
   const [name, setName] = useState()
   const [account, setAccount] = useState()
   const [age, setAge] = useState()
+  const [genderOption, setGenderOption] = useState()
   const [gender, setGender] = useState()
   const [credit, setCredit] = useState(100)
   const [isStudent, setIsStudent] = useState(false)
   const[isOpen, setIsOpen] = useState(false)
-
-  const [birthDate, setBirthDate] = useState()
+  const [birthDate, setBirthDate] = useState(new Date())
   
-
   const inputRef = useRef(null) //O useRef funcina de um jeito similar a ID do HTML, possibilitando manipular o componente diretamente
  
   function setAccountName(text)
@@ -39,11 +38,7 @@ export default function App()
     }
   }
 
-  function pickDate(date)
-  {
-    setBirthDate(date)
-    setIsOpen(false)
-  }
+  
 
  return (
    <View>
@@ -55,26 +50,44 @@ export default function App()
       placeholder = "Nome completo" 
       inlineImageLeft= 'person_icon'
       autoCapitalize= 'words'
-      onChangeText={(text) => myName = text}
+      //onChangeText={(text) => myName = text}
       />
 
       <PressableField 
-      onPress = {() =>{setIsOpen(true)}}/>
+      style = {styles.pickerButton}
+      onPress = {() =>{
+        setIsOpen(true)
+        console.log('ola')}}/>
+      
       
       <DatePicker
       modal
+      date={new Date()}
       open = {isOpen}
-      onConfirm = {(date) =>{pickDate(date)}}
+      onConfirm = {(date)=>{
+        setBirthDate(date)
+        setIsOpen(false)}}
       onCancel = {()=> setIsOpen(false)}
       mode = 'date'
       maximumDate={new Date("2004-01-01")}
       />
-
       
+      <Picker
+      selectedValue={genderOption}
+      onValueChange = {(value, index) =>{
+        setGenderOption(value)
+        setGender(value)
+        //console.log(gender+"\n" +genderOption)
+      }}>
+        <Picker.Item key ={0} value ={null} label = "Selecione um gênero"/>
+        <Picker.Item key ={1} value ={"Masculino"} label = "Masculino"/>
+        <Picker.Item key ={2} value ={"Feminino"} label = "Feminino"/>
+      </Picker>
 
-
+      <Text>{genderOption}</Text>
+      <Text>{gender}</Text>
       <Button title='Concluir' onPress={() =>{
-        alert(myName)
+        //alert(myName)
         inputRef.current.clear()
       }}/>
       
@@ -91,5 +104,9 @@ const styles = StyleSheet.create({
     height:60,
     borderWidth:0.5,
     borderRadius:20
+  },
+  pickerButton: {
+    width:200,
+    height:60
   }
 })
--- src/Components/PressableField.js
+++ src/Components/PressableField.js
@@ -3,18 +3,21 @@ import { View,Text, Pressable, StyleSheet} from 'react-native';
 
 export default function PressableField(props) {
  return (
-    <Pressable onPress={this.props.onPress}>
-        <View>
+    <Pressable 
+    onPress={props.onPress} 
+    style ={[styles.container, props.style]}>      
             <Text>
                 Ola
             </Text>
-        </View>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
     container:{
-
+        justifyContent:'center', 
+        alignItems:'center', 
+        backgroundColor:'green',
+        borderWidth:0.5
     }
 })
